fa5cb5a54869578eb786643cdf3c4a46cc049679
cybanjar <cybanjar@gmail.com>
2021-10-10 16:41:03 +0700

    first commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import Tabs from '../views/Tabs.vue'
+import Splash from '../views/Splash.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/tabs/tab1'
+    redirect: '/splash'
   },
   {
     path: '/tabs/',
@@ -28,6 +29,11 @@ const routes: Array<RouteRecordRaw> = [
         component: () => import('@/views/Tab3.vue')
       }
     ]
+  },
+  {
+    path: '/splash',
+    name: 'Splash',
+    component: Splash
   }
 ]
 
